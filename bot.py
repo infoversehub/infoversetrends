3b6983eefcfa616f388df3079dac04a6b66e2301
bwdalhbwb74-droid <bwdalhbwb74@gmail.com>
2026-06-21 09:28:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,37 @@
-from pytrends.request import TrendReq
 import requests
+import feedparser
 
 TOKEN = "8763023216:AAGTxJFJD2dnMtBHirSdx_fMhpyszuOkmS0"
 CHAT_ID = "7330431242"
 
 try:
-    pytrends = TrendReq(hl='en-US', tz=360)
+    arabic = feedparser.parse(
+        "https://news.google.com/rss?hl=ar&gl=SA&ceid=SA:ar"
+    )
 
-    trending_en = pytrends.trending_searches(pn='united_states')[0].head(5).tolist()
+    english = feedparser.parse(
+        "https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en"
+    )
 
-    message = "🌍 5 English Trends\n\n"
+    msg = "🔥 الترندات اليومية\n\n"
 
-    for i, trend in enumerate(trending_en, 1):
-        message += f"{i}- {trend}\n"
+    msg += "🇸🇦 5 مواضيع عربية:\n"
+    for i, item in enumerate(arabic.entries[:5], 1):
+        msg += f"{i}- {item.title}\n"
+
+    msg += "\n🌍 5 مواضيع إنجليزية:\n"
+    for i, item in enumerate(english.entries[:5], 1):
+        msg += f"{i}- {item.title}\n"
 
     requests.post(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
         data={
             "chat_id": CHAT_ID,
-            "text": message
+            "text": msg
         }
     )
 
-    print("Trends sent")
+    print("Sent successfully")
 
 except Exception as e:
     print(e)
