dce08ea9f36aef46a14759f1fac078be6961920f
bwdalhbwb74-droid <bwdalhbwb74@gmail.com>
2026-06-22 01:52:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,93 +4,53 @@ import feedparser
 TOKEN = "8763023216:AAGTxJFJD2dnMtBHirSdx_fMhpyszuOkmS0"
 CHAT_ID = "7330431242"
 
-arabic_sources = [
-    "https://news.google.com/rss?hl=ar&gl=SA&ceid=SA:ar",
-    "https://arabic.cnn.com/rss",
+feeds_ar = [
+    "https://www.tech-wd.com/wd/feed/",
+    "https://aitnews.com/feed/",
+    "https://www.arageek.com/feed"
 ]
 
-english_sources = [
-    "https://news.google.com/rss/search?q=Artificial+Intelligence&hl=en-US&gl=US&ceid=US:en",
-    "https://news.google.com/rss/search?q=Technology&hl=en-US&gl=US&ceid=US:en",
-    "https://news.google.com/rss/search?q=Cars&hl=en-US&gl=US&ceid=US:en",
-    "https://news.google.com/rss/search?q=Business&hl=en-US&gl=US&ceid=US:en",
-    "https://news.google.com/rss/search?q=Gaming&hl=en-US&gl=US&ceid=US:en",
-    "https://news.google.com/rss/search?q=Sports&hl=en-US&gl=US&ceid=US:en",
-    "https://news.google.com/rss/search?q=Smartphones&hl=en-US&gl=US&ceid=US:en",
-    "https://news.google.com/rss/search?q=World&hl=en-US&gl=US&ceid=US:en",
+feeds_en = [
+    "https://techcrunch.com/feed/",
+    "https://www.theverge.com/rss/index.xml",
+    "https://feeds.feedburner.com/venturebeat/SZYF",
+    "https://www.ign.com/rss",
+    "https://www.businessinsider.com/rss"
 ]
-
-used_words = set()
-
-def is_duplicate(title):
-    words = set(
-        w.lower()
-        for w in title.split()
-        if len(w) > 4
-    )
-
-    overlap = len(words & used_words)
-
-    if overlap >= 2:
-        return True
-
-    used_words.update(words)
-    return False
-
-
-arabic_topics = []
-english_topics = []
-
-for source in arabic_sources:
-    try:
-        feed = feedparser.parse(source)
-
-        for item in feed.entries:
-            title = item.title.split(" - ")[0]
-
-            if not is_duplicate(title):
-                arabic_topics.append(title)
-
-            if len(arabic_topics) >= 5:
-                break
-
-        if len(arabic_topics) >= 5:
-            break
-
-    except:
-        pass
-
-
-for source in english_sources:
-    try:
-        feed = feedparser.parse(source)
-
-        for item in feed.entries:
-            title = item.title.split(" - ")[0]
-
-            if not is_duplicate(title):
-                english_topics.append(title)
-
-            if len(english_topics) >= 5:
-                break
-
-        if len(english_topics) >= 5:
-            break
-
-    except:
-        pass
-
 
 message = "🔥 مواضيع اليوم للمقالات\n\n"
 
-message += "🇸🇦 5 مواضيع عربية\n\n"
+# عربي
+message += "🇸🇦 5 مواضيع عربية:\n\n"
 
-for i, topic in enumerate(arabic_topics[:5], 1):
+arab_topics = []
+for feed_url in feeds_ar:
+    try:
+        feed = feedparser.parse(feed_url)
+        for item in feed.entries[:5]:
+            title = item.title.strip()
+            if title not in arab_topics:
+                arab_topics.append(title)
+    except:
+        pass
+
+for i, topic in enumerate(arab_topics[:5], 1):
     message += f"{i}- {topic}\n"
 
-message += "\n🌍 5 مواضيع إنجليزية\n\n"
+message += "\n🌍 5 مواضيع إنجليزية:\n\n"
 
-for i, topic in enumerate(english_topics[:5], 1):
+eng_topics = []
+for feed_url in feeds_en:
+    try:
+        feed = feedparser.parse(feed_url)
+        for item in feed.entries[:10]:
+            title = item.title.strip()
+            if title not in eng_topics:
+                eng_topics.append(title)
+    except:
+        pass
+
+for i, topic in enumerate(eng_topics[:5], 1):
     message += f"{i}- {topic}\n"
 
 requests.post(
